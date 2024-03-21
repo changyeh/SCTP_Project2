4d48ba7a7270c653916b9f332f837099a58d8ce0
ChangYeh <changyeh@unicorn.sg>
2024-03-21 22:14:47 +0800

    use cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const hbs = require("hbs");
 const wax = require("wax-on");
+const cors = require("cors");
 require("dotenv").config();
 
 // create an instance of express app
@@ -22,6 +23,9 @@ app.use(
         extended: false
     })
 );
+
+// enable cors
+app.use(cors());
 
 const landingRoutes = require('./routes/landing');
 const catRoutes = require('./routes/cats');
